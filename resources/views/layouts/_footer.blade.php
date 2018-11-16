5dc2a23a37a24d031033c895701a9d1df22a4e27
vagrant <vagrant@homestead>
2018-11-16 06:22:47 +0000

    完成首頁

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://github.com/undertaker9104" target="_blank">HSING-YING</a> 設計和編碼 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="undertaker9104@email.com">聯繫我們</a></p>
+    </div
+</footer>
---
+++ resources/views/pages/root.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('title', '首页')
+
+@section('content')
+  <h1>這裡是首頁</h1>
+@stop
