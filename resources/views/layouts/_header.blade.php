a577ef5ec2db2f71ac9a1e096eb3da635d8c77c1
vagrant <vagrant@homestead>
2018-11-16 07:05:36 +0000

    新增註冊驗證碼功能

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -26,8 +26,8 @@
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @guest
-                    <li><a href="{{ route('login') }}">登录</a></li>
-                    <li><a href="{{ route('register') }}">注册</a></li>
+                    <li><a href="{{ route('login') }}">登錄</a></li>
+                    <li><a href="{{ route('register') }}">註冊</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -42,7 +42,7 @@
                                 <a href="{{ route('logout') }}"
                                     onclick="event.preventDefault();
                                              document.getElementById('logout-form').submit();">
-                                    退出登录
+                                    退出登錄
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
