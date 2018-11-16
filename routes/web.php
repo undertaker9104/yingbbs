137725fb28e44b218bbb5a135f8d0379acd7e0e2
vagrant <vagrant@homestead>
2018-11-16 07:27:26 +0000

    用戶簡介功能完成

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail'
 Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
 Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
+//Users
+Route::resource('users', 'UsersController',['Only' => ['show','update','edit']]);
+
+
