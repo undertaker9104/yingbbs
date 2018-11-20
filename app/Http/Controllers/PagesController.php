04c9318abebf24f26da91d1a36a47cb4101675be
vagrant <vagrant@homestead>
2018-11-20 12:20:50 +0000

    後台權限分配

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,5 +8,14 @@ class PagesController extends Controller
 {
 	public function root() {
 		return view('pages.root');
-	}
+    }
+
+    public function permissionDenied()
+    {
+        if(config('administrator.permission')()) {
+            return redirect(url(config('administrator.uri')),302);
+        }
+
+        return view('pages.permission_denied');
+    }
 }
