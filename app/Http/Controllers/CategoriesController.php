dcf76a38c5189df427c6307d512a8386c73cae49
vagrant <vagrant@homestead>
2018-11-24 07:12:04 +0000

    活躍用戶功能

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Topic;
+use App\Models\User;
 class CategoriesController extends Controller
 {
-    public function show(Category $category,Request $request, Topic $topic) {
+    public function show(Category $category,Request $request, Topic $topic, User $user) {
         // 讀取分類 ID 關聯的話題，並按照每 20 調分頁
         $topics = $topic->withOrder($request->order)->where('category_id',$category->id)->paginate(20);
 
-        return view('topics.index',compact('topics','category'));
+        $active_users = $user->getActiveUsers();
+        return view('topics.index',compact('topics','category','active_users'));
 
     }
 }
