b0197762f2992a50c511e8a2e85079be789c7680
vagrant <vagrant@homestead>
2018-11-29 01:46:24 +0000

    user update

--- routes/api.php
+++ routes/api.php
@@ -46,4 +46,26 @@ $api->version('v1', [
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
         ->name('api.authorizations.destroy');
     });
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.access.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 游客可以访问的接口
+
+        // 需要 token 验证的接口
+        $api->group(['middleware' => 'api.auth'], function($api) {
+            // 当前登录用户信息
+            $api->get('user', 'UsersController@me')
+                ->name('api.user.show');
+            // 图片资源
+            $api->post('images', 'ImagesController@store')
+            ->name('api.images.store');
+            // 编辑登录用户信息
+            $api->patch('user', 'UsersController@update')
+            ->name('api.user.update');
+        });
+    });
 });
+
+
