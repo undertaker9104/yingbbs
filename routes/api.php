a4aa76413b47951cb5baef117985f7ed83ad7fb5
vagrant <vagrant@homestead>
2018-12-01 02:16:22 +0000

    notifications index

--- routes/api.php
+++ routes/api.php
@@ -91,6 +91,9 @@ $api->version('v1', [
             // 删除回复
             $api->delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')
             ->name('api.topics.replies.destroy');  
+            // 通知列表
+            $api->get('user/notifications', 'NotificationsController@index')
+            ->name('api.user.notifications.index');
             // 编辑登录用户信息
             $api->patch('user', 'UsersController@update')
             ->name('api.user.update');
