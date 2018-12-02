092dc30ca4eeefcbbf9c2a4f4141f6e95bac4810
vagrant <vagrant@homestead>
2018-12-02 03:31:31 +0000

    自己客戶端的passport授權模式

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Carbon\Carbon;
+use Laravel\Passport\Passport;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -25,12 +26,19 @@ class AuthServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        $this->registerPolicies();
+{
+    $this->registerPolicies();
 
-        \Horizon::auth(function ($request) {
-            //判斷是否是站長
-            return \Auth::user()->hasRole('Founder');
-        });
-    }
+    // Passport 的路由
+    Passport::routes();
+    // access_token 过期时间
+    Passport::tokensExpireIn(Carbon::now()->addDays(15));
+    // refreshTokens 过期时间
+    Passport::refreshTokensExpireIn(Carbon::now()->addDays(30));
+
+    \Horizon::auth(function ($request) {
+        // 是否是站长
+        return \Auth::user()->hasRole('Founder');
+    });
+}
 }
